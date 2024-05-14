8a281a1d73684b4f177e14074266ac952469d6db
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-05-14 14:49:04 -0300

    build: initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, CriarCursoModule, CursosModule],
+  providers: [CursosService, LogService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
